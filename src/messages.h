18f69934136672b6593aa69990ecaf73d5689fb8
Sophie 'Sharky' Schumann <sharky@sharky.pw>
2019-04-22 02:49:23 +0200

    more messages

--- src/messages.h
+++ src/messages.h
@@ -1,7 +1,16 @@
 #include <wchar.h>
 
 static const wchar_t * success_msgs[] = {
-    L"Error: Operation completed successfully!"
+    L"Error: Operation completed successfully!",
+	L"An error has occured while creating an error report.",
+	L"What did the laptop say to the dentist?\r\nMy bluetooth hurts!\r\nNo, but seriously, that albino midget porn is ridden with malware, maybe you should try normal people porn.",
+	L"Task failed successfully.",
+	L"An error occured while displaying the previous error.",
+	L"No error occured.",
+	L"Failed to print document!\r\nPrinting is not supported on this printer.",
+	L"You've been warned 3 times that this file does not exist.\r\nNow you've made us catch this worthless exception and we're upset.\r\nDo not do this again.",
+	L"No error today, I just wanted to tell you that you look gorgeous!",
+	L"Something happened."
 };
 
-static const unsigned int success_msgs_len = 1;
+static const unsigned int success_msgs_len = 10;
